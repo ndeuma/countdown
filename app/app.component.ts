10948be64ea4db12d6a20e24dc74ebad2d0e9b68
Niklas Deutschmann <mail@niklas-deutschmann.de>
2016-07-23 18:44:42 +0200

    Extracted Event class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
+import { Event } from './event';
 @Component({
   selector: 'countdown-app',
   templateUrl: '/app/app.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  events = [
+    new Event("Stadtlauf Nürnberg", "72 Tage, 21:56:10"),
+    new Event("Frankfurt-Marathon", "100 Tage, 14:04:33"),
+  ]
+}
---
+++ app/event.ts
@@ -0,0 +1,5 @@
+export class Event {
+  constructor(
+    public name: string,
+    public timeRemaining: string) { }
+}
