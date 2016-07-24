fba56f99167cda1a42f87e59e70b07fb09eb7459
Niklas Deutschmann <mail@niklas-deutschmann.de>
2016-07-24 08:04:46 +0200

    Addition and deletion of events (no user input yet)

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,5 +8,14 @@ export class AppComponent {
   events = [
     new Event("Stadtlauf Nürnberg", "72 Tage, 21:56:10"),
     new Event("Frankfurt-Marathon", "100 Tage, 14:04:33"),
-  ]
+  ];
+
+  addEvent() {
+    this.events.push(new Event("Some other event", "xx days, yy:yy:yy"));
+  }
+
+  deleteEvent(event:Event) {
+    var index = this.events.indexOf(event);
+    this.events.splice(index, 1);
+  }
 }
